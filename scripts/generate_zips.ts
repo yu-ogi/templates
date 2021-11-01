02c3b459f774f8417591aba0deb7fe0b5688e37e
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2021-11-01 14:19:02 +0900

    feat: add workflows (deploy, release)

--- scripts/generate_zips.ts
+++ scripts/generate_zips.ts
@@ -3,10 +3,21 @@ import * as path from "path";
 import * as util from "util";
 import * as childProcess from "child_process";
 
+interface TemplateData {
+	filepath: string;
+	list: TemplateList;
+}
+
+interface TemplateList {
+	templates: {[name: string]: string};
+}
+
 const exec = util.promisify(childProcess.exec);
 
 const outputDir = path.join(__dirname, "..", "dist");
 const templateDir = path.join(__dirname, "..", "templates");
+const templateListJsonDir = path.join(__dirname, "..", "public");
+const templateListJsonBaseUrl = "https://github.com/akashic-contents/templates/releases/latest/download/";
 
 (async () => {
 	const templatePaths = (await fs.readdir(templateDir, { withFileTypes: true }))
@@ -21,9 +32,21 @@ const templateDir = path.join(__dirname, "..", "templates");
 		await fs.unlink(existsZip);
 	}
 
+	const templateData: TemplateData = {
+		filepath: path.join(templateListJsonDir, "template-list.json"),
+		list: {
+			templates: {}
+		}
+	};
+
 	for (let templatePath of templatePaths) {
+		const templateName = path.basename(templatePath);
+		const templateZipName = `${templateName}.zip`;
 		await generateZip(templateDir, templatePath, `${path.basename(templatePath)}.zip`, outputDir);
+		templateData.list.templates[templateName] = templateListJsonBaseUrl + templateZipName;
 	}
+
+	await writeTemplateList(templateData);
 
 	console.log("generated template zips successfully.");
 })().catch(e => {
@@ -34,4 +57,8 @@ const templateDir = path.join(__dirname, "..", "templates");
 async function generateZip(cwd: string, dir: string, name: string, output: string): Promise<any> {
 	await exec(`zip -r ${name} ${dir}`, { cwd, encoding: "utf-8" });
 	await exec(`mv ${name} ${output}`, { cwd, encoding: "utf-8" });
+}
+
+async function writeTemplateList({ filepath, list }: TemplateData): Promise<void> {
+	await fs.writeFile(filepath, JSON.stringify(list, undefined, 2));
 }
