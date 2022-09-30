adc842db40849802e64af2de1d290fa2051b7f76
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2022-09-30 15:21:09 +0900

    chore: ignore *.js

--- templates/typescript-shin-ichiba-ranking/.eslintrc.js
+++ templates/typescript-shin-ichiba-ranking/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
   extends: "@akashic/eslint-config",
+  ignorePatterns: [
+    "**/*.js"
+  ],
   parserOptions: {
     project: "tsconfig.eslint.json",
     sourceType: "module"
