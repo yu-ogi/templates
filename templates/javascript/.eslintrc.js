bcc40d3f9dbf198d40f916e42fb1ebc2f3e3e6f2
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2022-09-30 14:04:44 +0900

    chore: use @akashic/eslint-config

--- templates/javascript/.eslintrc.js
+++ templates/javascript/.eslintrc.js
@@ -1,26 +1,8 @@
-{
-  "env": {
-    "commonjs": true
-  },
-  "parserOptions": {
-    "ecmaVersion": 5
-  },
-  "globals": {
-    "window": false,
-    "RPGAtsumaru": false,
-    "g": false
-  },
-  "rules": {
-    "no-dupe-args": "error",
-    "no-dupe-keys": "error",
-    "no-duplicate-case": "error",
-    "no-inner-declarations": "error",
-    "no-irregular-whitespace": ["error", {
-      "skipStrings": true,
-      "skipComments": true,
-      "skipRegExps": true,
-      "skipTemplates": true
-    }],
-    "no-undef": "error"
+module.exports = {
+  root: true,
+  extends: "@akashic/eslint-config",
+  parserOptions: {
+    project: "tsconfig.eslint.json",
+    sourceType: "module"
   }
 }
